a3d26ade75ce20b239a4682a17b91738013f9cea
DavidSatria29 <alamsdavsat@gmail.com>
2023-03-16 05:08:19 +0700

    ubah f

--- jantung-app/app/Http/Controllers/KmeansController.php
+++ jantung-app/app/Http/Controllers/KmeansController.php
@@ -9,8 +9,42 @@ class KmeansController extends Controller
 {
     public function kmeans()
     {
+        $data = DB::table('data')->get();
+
+        foreach ($data as $d) {
+            $kolom1 = $d->kolom1;
+            $kolom2 = $d->kolom2;
+            $kolom3 = $d->kolom3;
+
+            // Menghitung jarak antara data dengan setiap cluster
+            $center1id1 = DB::table('cluster')->where('id', 1)->value('center1');
+            $center2id1 = DB::table('cluster')->where('id', 1)->value('center2');
+            $center3id1 = DB::table('cluster')->where('id', 1)->value('center3');
+
+            $jarak1 = sqrt(pow(($kolom1 - $center1id1), 2) + pow(($kolom2 - $center2id1), 2) + pow(($kolom3 - $center3id1), 2));
+
+            $center1id2 = DB::table('cluster')->where('id', 2)->value('center1');
+            $center2id2 = DB::table('cluster')->where('id', 2)->value('center2');
+            $center3id2 = DB::table('cluster')->where('id', 2)->value('center3');
+
+            $jarak2 = sqrt(pow(($kolom1 - $center1id2), 2) + pow(($kolom2 - $center2id2), 2) + pow(($kolom3 - $center3id2), 2));
+
+            // Menentukan cluster yang terdekat dengan data
+            if ($jarak1 <= $jarak2) {
+                $out = 1;
+            } else {
+                $out = 2;
+            }
+
+            // Menyimpan hasil clustering pada tabel data
+            DB::table('data')->where('id', $d->id)->update(['C1' => $jarak1, 'C2' => $jarak2, 'Class' => $out]);
+
+            // Menambah jumlah anggota pada cluster yang terpilih
+            DB::table('cluster')->where('id', $out)->increment('jumlahAnggota');
+        }
         $maxIterasi = 10;
         $iterasi = 0;
+
 
         // Melakukan pengulangan untuk setiap iterasi
         while ($iterasi < $maxIterasi) {
@@ -24,9 +58,9 @@ class KmeansController extends Controller
             $mean3 = DB::table('data')->where('id', 3)->avg('kolom3');
 
             // Mengupdate nilai tengah pada setiap cluster dengan rata-rata terbaru
-            // DB::table('cluster')->where('id', 1)->update(['center1' => $mean1, 'center2' => $mean2, 'center3' => $mean3]);
-            // DB::table('cluster')->where('id', 2)->update(['center1' => $mean1, 'center2' => $mean2, 'center3' => $mean3]);
-            // DB::table('cluster')->where('id', 3)->update(['center1' => $mean1, 'center2' => $mean2, 'center3' => $mean3]);
+            DB::table('cluster')->where('id', 1)->update(['center1' => $mean1, 'center2' => $mean2, 'center3' => $mean3]);
+            DB::table('cluster')->where('id', 2)->update(['center1' => $mean1, 'center2' => $mean2, 'center3' => $mean3]);
+            DB::table('cluster')->where('id', 3)->update(['center1' => $mean1, 'center2' => $mean2, 'center3' => $mean3]);
 
             // Mengambil data pada tabel data
             $data = DB::table('data')->get();
