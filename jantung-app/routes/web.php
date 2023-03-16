61c8858a283f6b4a31d751e611f297cd742bd16e
DavidSatria29 <alamsdavsat@gmail.com>
2023-03-16 19:50:43 +0700

    done kmeans

--- jantung-app/routes/web.php
+++ jantung-app/routes/web.php
@@ -23,10 +23,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/kmeans', [KmeansController::class, 'kmeans'])->name('kmeans');
-Route::get('/cn', [KmeansController::class, 'cn']);
-Route::get('/km', [KmeansController::class, 'km']);
-Route::get('/cek', [KmeansController::class, 'cek']);
-
 Route::get('/bayes', function () {
     return view('user.bayes');
 });
