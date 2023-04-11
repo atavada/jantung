a31d716e49a17847aa89ad0b1b95d01429d5ef91
ardha2321 <ardhaardhanaputra@gmail.com>
2023-04-11 16:44:30 +0700

    ubah jadi kmean

--- jantung-app/database/migrations/2023_04_11_063434_create_data_pilihan_akurasi.php
+++ jantung-app/database/migrations/2023_04_11_063434_create_data_pilihan_akurasi.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('data_pilihan_bayes', function (Blueprint $table) {
+        Schema::create('data_pilihan_kmean', function (Blueprint $table) {
             $table->id();
             $table->integer('trestbps');
             $table->integer('chol');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('data_pilihan_bayes');
+        Schema::dropIfExists('data_pilihan_kmean');
     }
 };
